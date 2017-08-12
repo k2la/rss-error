5f2e99eb5f3f8963dd986a9d5d25456bad9b610f
Yuto Furuta <mzq6mft9zz@gmail.com>
2017-08-12 20:52:52 +0900

    add values

---
+++ main.py
@@ -0,0 +1,20 @@
+from values import *
+import random
+
+width = 10
+height = 10
+random_width = random.choice(range(width))
+random_height = random.choice(range(height))
+
+if __name__ == '__main__':
+    random.shuffle(data['level1'])
+    correct_word = data['level1'][0][0]
+    dummy_word = data['level1'][0][1]
+    for w in range(width):
+        for h in range(height):
+            if w == random_width and h == random_height:
+                print(dummy_word, end="")
+            else:
+                print(correct_word, end="")
+        print()
+
